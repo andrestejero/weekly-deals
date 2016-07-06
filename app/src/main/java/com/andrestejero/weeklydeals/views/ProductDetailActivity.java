d79e4130ca7c3692e752cb22f228550c17b6915f
Andres Tejero <atejero@garbarino.com.ar>
2016-07-06 14:38:26 -0300

    Agrego detalle de producto

--- app/src/main/java/com/andrestejero/weeklydeals/views/ProductDetailActivity.java
+++ app/src/main/java/com/andrestejero/weeklydeals/views/ProductDetailActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.andrestejero.weeklydeals.AppBaseActivity;
 import com.andrestejero.weeklydeals.R;
 import com.andrestejero.weeklydeals.models.ProductDetail;
+import com.andrestejero.weeklydeals.network.ImageRequest;
 import com.andrestejero.weeklydeals.utils.StringUtils;
 import com.andrestejero.weeklydeals.views.presenters.ProductDetailPresenter;
 
@@ -62,7 +64,13 @@ public class ProductDetailActivity extends AppBaseActivity implements ProductDet
     public void showProductDetail(@NonNull ProductDetail productDetail) {
         if (mViewHolder != null) {
             updateLoadingView(mViewHolder.loadingView, View.GONE);
-            mViewHolder.title.setText(productDetail.getDescription());
+            if (StringUtils.isNotEmpty(productDetail.getImage())) {
+                new ImageRequest(this, productDetail.getImage(), mViewHolder.defaultImage).widthInPixels(200, 1000).execute();
+            } else {
+                mViewHolder.defaultImage.setImageResource(R.drawable.bg_image_placeholder_200dp);
+            }
+            mViewHolder.name.setText(productDetail.getName());
+            mViewHolder.provider.setText(productDetail.getProvider());
         }
     }
 
@@ -73,11 +81,15 @@ public class ProductDetailActivity extends AppBaseActivity implements ProductDet
 
     private class ViewHolder {
         private View loadingView;
-        private final TextView title;
+        private final ImageView defaultImage;
+        private final TextView name;
+        private final TextView provider;
 
         private ViewHolder() {
             loadingView = findViewById(R.id.loadingView);
-            title = (TextView) findViewById(R.id.tvGameDetail);
+            defaultImage = (ImageView) findViewById(R.id.ivDetailImage);
+            name = (TextView) findViewById(R.id.tvDetailName);
+            provider = (TextView) findViewById(R.id.tvDetailProvider);
         }
     }
 }
